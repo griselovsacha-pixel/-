298208cf45c52152c4ac85e5dab958747db67cc9
griselovsacha-pixel <griselovsacha@gmail.com>
2026-04-21 12:32:49 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,3 @@
 TOKEN = "8624231560:AAFOk3q54Dl2iX38M1m9uMlUf8sBChE44EY"
-OWNER_ID = 880628963  # Твій ID
+OWNER_ID = 880628963
+DB_URL = "postgres://nail_studio_db_user:XDbYKFDJhQva1sjgpDpnR57zFGU1Z0u3@dpg-d7jk692qqhas738h4mig-a/nail_studio_db"
