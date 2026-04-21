783784fe803d666910500abd606a7be005d1678d
griselovsacha-pixel <griselovsacha@gmail.com>
2026-04-21 16:32:36 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
 TOKEN = "8624231560:AAFOk3q54Dl2iX38M1m9uMlUf8sBChE44EY"
 OWNER_ID = 880628963
 DB_URL = "postgresql://nail_studio_db_user:XDbYKFDJhQva1sjgpDpnR57zFGU1Z0u3@dpg-d7jk692qqhas738h4mig-a/nail_studio_db"
-CHANNEL_ID = "@DaynSeka" 
-CHANNEL_LINK = "https://t.me/DaynSeka"
+
